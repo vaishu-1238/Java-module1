c71ba14298a11beb85a0687a493b9617317bc1bb
vaishu-1238 <vaishnavideshpande995@gmail.com>
2025-08-25 12:07:46 +0530

    Add files via upload
    it gives addition of two numbers

---
+++ add.java
@@ -0,0 +1,14 @@
+import java.util.*;
+ public class add {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two numbers:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a+b;
+        System.out.println("Addition of two number is " +c);
+    }
+    
+ }
+    
+
