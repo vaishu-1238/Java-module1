66b5a0c44c01650a4344d94b34e5ac8c402659e9
vaishu-1238 <vaishnavideshpande995@gmail.com>
2025-09-01 21:40:46 +0530

    Add files via upload

---
+++ upper.java
@@ -0,0 +1,14 @@
+public class upper {
+    public static void main(String[] args) {
+        String stra="Hello java";
+        String strb="hello java";
+        System.out.println(stra.toUpperCase());
+        System.out.println(stra.toLowerCase());
+        System.out.println(stra.length());
+        System.out.println(stra.charAt(4));
+        if(stra.equals(strb))
+        System.out.println("stra and strb are equal");
+        else
+        System.out.println("stra and strb are not equal");
+    }
+}
