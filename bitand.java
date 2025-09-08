20e33c960b090bbc97e2926148c440f283c618a4
vaishu-1238 <vaishnavideshpande995@gmail.com>
2025-09-08 12:20:38 +0530

    Add files via upload

---
+++ bitand.java
@@ -0,0 +1,7 @@
+public class bitand {
+    public static void main(String[] args) {
+        int a=12;
+        int b=10;
+        System.out.println("a & b="+(a & b));
+    }
+}
---
+++ bitor.java
@@ -0,0 +1,7 @@
+public class bitor {
+    public static void main(String[] args) {
+        int a=12;
+        int b=19;
+        System.out.println("a | b :"+(a | b));
+    }
+}
